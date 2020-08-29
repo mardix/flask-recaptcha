f30820ffefd06a80f1633bba7642e99ec5d5812f
Max Levine <max@maxlevine.co.uk>
2020-08-29 17:41:38 +0100

    Make generic for any compatible recaptcha equivalent
    Defaulting to Google reCaptcha

--- flask_recaptcha.py
+++ flask_recaptcha.py
@@ -23,17 +23,31 @@ class DEFAULTS(object):
     TYPE = "image"
     SIZE = "normal"
     TABINDEX = 0
+    VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify"
+    API_URL = "//www.google.com/recaptcha/api.js"
+    DIV_CLASS = "g-recaptcha"
 
 
 class ReCaptcha(object):
 
-    VERIFY_URL = "https://www.google.com/recaptcha/api/siteverify"
     site_key = None
     secret_key = None
     is_enabled = False
 
     def __init__(self, app=None, site_key=None, secret_key=None, is_enabled=True, **kwargs):
-        if site_key:
+        if app is not None:
+            self.site_key = app.config.get("RECAPTCHA_SITE_KEY") if site_key is None else site_key
+            self.secret_key = app.config.get('RECAPTCHA_SECRET_KEY') if secret_key is None else secret_key
+            self.is_enabled = app.config.get("RECAPTCHA_ENABLED", DEFAULTS.IS_ENABLED) if is_enabled is None else is_enabled
+            self.theme = app.config.get("RECAPTCHA_THEME", DEFAULTS.THEME) if kwargs.get('theme') is None else kwargs.get('theme')
+            self.type = app.config.get("RECAPTCHA_TYPE", DEFAULTS.TYPE) if kwargs.get('type') is None else kwargs.get('type')
+            self.size = app.config.get("RECAPTCHA_SIZE", DEFAULTS.SIZE) if kwargs.get('size') is None else kwargs.get('size')
+            self.tabindex = app.config.get("RECAPTCHA_TABINDEX", DEFAULTS.TABINDEX) if kwargs.get('tabindex') is None else kwargs.get('tabindex')
+            self.verify_url = app.config.get("RECAPTCHA_VERIFY_URL", DEFAULTS.VERIFY_URL) if kwargs.get('verify_url') is None else kwargs.get('verify_url')
+            self.api_url = app.config.get("RECAPTCHA_API_URL", DEFAULTS.API_URL) if kwargs.get('api_url') is None else kwargs.get('api_url')
+            self.div_class = app.config.get("RECAPTCHA_DIV_CLASS", DEFAULTS.DIV_CLASS) if kwargs.get('div_class') is None else kwargs.get('div_class')
+
+        elif site_key is not None:
             self.site_key = site_key
             self.secret_key = secret_key
             self.is_enabled = is_enabled
@@ -41,18 +55,9 @@ class ReCaptcha(object):
             self.type = kwargs.get('type', DEFAULTS.TYPE)
             self.size = kwargs.get('size', DEFAULTS.SIZE)
             self.tabindex = kwargs.get('tabindex', DEFAULTS.TABINDEX)
-
-        elif app:
-            self.init_app(app=app)
-
-    def init_app(self, app=None):
-        self.__init__(site_key=app.config.get("RECAPTCHA_SITE_KEY"),
-                      secret_key=app.config.get("RECAPTCHA_SECRET_KEY"),
-                      is_enabled=app.config.get("RECAPTCHA_ENABLED", DEFAULTS.IS_ENABLED),
-                      theme=app.config.get("RECAPTCHA_THEME", DEFAULTS.THEME),
-                      type=app.config.get("RECAPTCHA_TYPE", DEFAULTS.TYPE),
-                      size=app.config.get("RECAPTCHA_SIZE", DEFAULTS.SIZE),
-                      tabindex=app.config.get("RECAPTCHA_TABINDEX", DEFAULTS.TABINDEX))
+            self.verify_url = kwargs.get('verify_url', DEFAULTS.VERIFY_URL)
+            self.api_url = kwargs.get('api_url', DEFAULTS.API_URL)
+            self.div_class = kwargs.get('div_class', DEFAULTS.DIV_CLASS)
 
         @app.context_processor
         def get_code():
@@ -64,19 +69,28 @@ class ReCaptcha(object):
         :return:
         """
         return "" if not self.is_enabled else ("""
-        <script src='//www.google.com/recaptcha/api.js'></script>
-        <div class="g-recaptcha" data-sitekey="{SITE_KEY}" data-theme="{THEME}" data-type="{TYPE}" data-size="{SIZE}"\
+        <script src='{API_URL}'></script>
+        <div class="{DIV_CLASS}" data-sitekey="{SITE_KEY}" data-theme="{THEME}" data-type="{TYPE}" data-size="{SIZE}"\
          data-tabindex="{TABINDEX}"></div>
-        """.format(SITE_KEY=self.site_key, THEME=self.theme, TYPE=self.type, SIZE=self.size, TABINDEX=self.tabindex))
+        """.format(
+            DIV_CLASS=self.div_class,
+            API_URL=self.api_url,
+            SITE_KEY=self.site_key,
+            THEME=self.theme,
+            TYPE=self.type,
+            SIZE=self.size,
+            TABINDEX=self.tabindex
+            )
+        )
 
     def verify(self, response=None, remote_ip=None):
         if self.is_enabled:
             data = {
                 "secret": self.secret_key,
-                "response": response or request.form.get('g-recaptcha-response'),
+                "response": response or request.form.get(f'{self.div_class}-response'),
                 "remoteip": remote_ip or request.environ.get('REMOTE_ADDR')
             }
 
-            r = requests.get(self.VERIFY_URL, params=data)
+            r = requests.get(self.verify_url, params=data)
             return r.json()["success"] if r.status_code == 200 else False
         return True
